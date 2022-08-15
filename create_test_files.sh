97be7491f166f06434e003d60c18fa7f9ac2fb32
Chernobyl81 <liurui81@gmail.com>
2022-08-15 19:06:52 +0800

    add python connect to postgresql

--- create_test_files.sh
+++ create_test_files.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
 input="./filelist.txt"
-while IFS = read -r line
+while IFS= read -r line
 do 
     touch "$line"
 done < "$input"
+
+# while IFS='-' read -ra DATE line
+# do 
+#     for i in "${DATE[@]}"; do
+#         echo $i
+#     done
+#     # touch "$line"
+#     # echo -n $line | md5sum | awk '{print $1}'
+# done < "$input"
+
+#rm *.mkv
