3a52671bb505f84742f14e0972cb4828db754877
Chernobyl81 <liurui81@gmail.com>
2022-08-30 13:47:47 +0800

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,6 @@ set +x
 path=${1:-/home/send/encrypt_hls}
 threshold=${2:-20}
 host=${3:-127.0.0.1}
+log_path=${4:/home/send/remove.log}
 
-echo "59 23 * * * root video_cleaner $path $threshold $host" >> /etc/crontab
+echo "59 23 * * * root video_cleaner $path $threshold $host $log_path" >> /etc/crontab
